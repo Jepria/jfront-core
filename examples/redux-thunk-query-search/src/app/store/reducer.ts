4286229a56a4313fbe9ed55f886f03f09924a0d2
AlexSav94 <aleksoider@gmail.com>
2021-02-19 18:16:27 +0300

    feat: search with query string and http get method

---
+++ examples/redux-thunk-query-search/src/app/store/reducer.ts
@@ -0,0 +1,40 @@
+import { EntityState, OptionState, SearchState } from "@jfront/core-redux-thunk";
+import { combineReducers, Reducer } from "@reduxjs/toolkit";
+import { initialEntityState, reducer as crudReducer } from "../../list/state/listCrudSlice";
+import { initialSearchState, reducer as searchReducer } from "../../list/state/listSearchSlice";
+import { initialOptionsState, reducer as optionsReducer } from "../../list/state/listOptionsSlice";
+import {
+  initialFilterOptionsState,
+  reducer as filterOptionsReducer,
+} from "../../list/state/listFilterOptionsSlice";
+import { Item } from "../../list/types";
+import { SearchTemplate } from "../../list/api/ListTypes";
+
+export interface AppState {
+  list: {
+    listSearchSlice: SearchState<SearchTemplate, Item>;
+    listCrudSlice: EntityState<Item>;
+    listOptionsSlice: OptionState<string>;
+    listFilterOptionsSlice: OptionState<string>;
+  };
+}
+
+export const initialState: AppState = {
+  list: {
+    listSearchSlice: initialSearchState,
+    listCrudSlice: initialEntityState,
+    listOptionsSlice: initialOptionsState,
+    listFilterOptionsSlice: initialFilterOptionsState,
+  },
+};
+
+const listReducer = combineReducers({
+  listSearchSlice: searchReducer,
+  listCrudSlice: crudReducer,
+  listOptionsSlice: optionsReducer,
+  listFilterOptionsSlice: filterOptionsReducer,
+});
+
+export const reducer: Reducer<AppState> = combineReducers<AppState>({
+  list: listReducer,
+});
