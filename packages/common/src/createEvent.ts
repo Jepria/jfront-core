4de13140b7fe94dddb4a71f71113b0e9c03ec3b0
AlexSav94 <aleksoider@gmail.com>
2021-03-19 15:47:45 +0300

    fix: create event not working in React 17 without cancelable and bubbles properties

--- packages/common/src/createEvent.ts
+++ packages/common/src/createEvent.ts
@@ -1,7 +1,7 @@
 export function createEvent(eventName: string) {
   let event;
   if (typeof Event === "function") {
-    event = new Event(eventName);
+    event = new Event(eventName, { cancelable: true, bubbles: true });
   } else {
     event = document.createEvent("Event");
     event.initEvent(eventName, true, true);
